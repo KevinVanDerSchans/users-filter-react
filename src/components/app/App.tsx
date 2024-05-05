63a034e310a6f2dcb0acc0d4601d6eac9b19a951
Kevin Schans <kevinvdsd@hotmail.com>
2024-05-05 13:50:13 +0200

    Create fetch

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,26 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api?results=100')
+      .then(async res => await res.json())
+      .then(res => {
+        setUsers(res.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
   return (
     <>
       <div className='app'>
         <h1>Users filter</h1>
+
+        {JSON.stringify(users)}
       </div>
     </>
   )
